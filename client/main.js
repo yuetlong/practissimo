c51e8018cd699e3fec0d17afbeb202bbbee75b8a
Zachary Wong <jerichozsx@gmail.com>
2016-04-05 19:46:18 -0400

    button animations and canvas

--- client/main.js
+++ client/main.js
@@ -49,6 +49,7 @@ Template.scheduler.onDestroyed(function(){
   scheduler.clearAll();
 });
 
+
 Template.main_menu.events({
   'click #start'(event){
     //event.preventDefault();
@@ -69,10 +70,10 @@ Template.body.helpers({
   currentPage: function(page){
     return Session.get('page')
   }
-})
+});
 
 Template.body.events({
   'click .btn': function(event, template){
     Session.set('page', event.currentTarget.getAttribute('data-page'))
   }
-})
+});

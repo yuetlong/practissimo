ea80094aedf4821d10beb73535ef4c3e7eeb0e73
yuetlong <yuetlong@icloud.com>
2016-04-12 17:08:52 -0400

    putting everything back together

--- imports/ui/clockpicker.js
+++ imports/ui/clockpicker.js
@@ -2,15 +2,27 @@ import { Template } from 'meteor/templating'
 import { ReactiveVar } from 'meteor/reactive-var'
 import { Session } from 'meteor/session'
 import { Meteor } from 'meteor/meteor'
+import { Blaze  } from 'meteor/blaze'
 
 import '../clockpicker/bootstrap-clockpicker.js'
 import './clockpicker.html'
 import '../pitchdetection/pitchdetect.js'
 
+Template.clockpicker.onCreated(function(){
+    this.currentPage = new ReactiveVar("timeForm");
+});
+
+Template.clockpicker.helpers({
+    currentPage(){
+        return Template.instance().currentPage.get();
+    }
+});
+
 Template.timeForm.onCreated(function (){
     // this is where you set the default values for the form
-    this.hours = new ReactiveVar(1);
-    this.minutes = new ReactiveVar(30);
+    this.hours = new ReactiveVar();
+    this.minutes = new ReactiveVar();
+    this.showForm = new ReactiveVar(true);
 });
 
 Template.timeForm.onRendered(function(){
@@ -18,9 +30,6 @@ Template.timeForm.onRendered(function(){
     $(".clockpicker").clockpicker({
         donetext: 'OK',
         autoclose : false,
-        default: 'now',
-        fromnow: Template.instance().hours.get() * 1000 * 60 * 60
-        + Template.instance().minutes.get() * 1000 * 60,
         twelvehour: true
     });
 });
@@ -43,11 +52,108 @@ Template.timeForm.helpers({
     },
     minutesEqualsOne(){
         return Template.instance().minutes.get() == 1;
+    },
+    hoursMinutesNotSet(){
+        var instance = Template.instance();
+        return (instance.hours.get() == null && instance.minutes.get() == null);
+    },
+    showForm(){
+        return Template.instance().showForm.get();
     }
 });
 
+Template.soundIndicator.helpers({
+    hasSound: function(){
+        return Session.get('noteElem') !== '-';
+    }
+});
+
+var initializeTimers = function(timestamp) {
+
+    var timeElapsed = moment.duration(0, 'seconds');
+    var timeRemains = moment.duration(timestamp.diff(moment()));
+    var interval = 200;
+    var tid = setInterval(startCounting, interval);
+
+    var timeElaspedInSeconds;
+    var timeElaspedInMinutes;
+    var timeElaspedInHours;
+
+    var timeRemainsInMinutes;
+    var timeRemainsInHours;
+
+    function te(string) {
+        $("#timeElapsed").html(string);
+    }
+
+    function tr(string) {
+        $("#timeRemains").html(string);
+    }
+
+    function startCounting() {
+
+        timeElaspedInSeconds = timeElapsed.seconds();
+        timeElaspedInMinutes = timeElapsed.minutes();
+        timeElaspedInHours = timeElapsed.hours();
+
+        if (timeElaspedInHours > 1) {
+            tr("You've practiced for " + timeElaspedInHours + " hours and " + timeElaspedInMinutes + " minutes! That's amazing!");
+        }
+        else if (timeElaspedInHours == 1) {
+            tr("You've practiced for an hour and " + timeElaspedInMinutes + " minutes!");
+        }
+        else if (timeElaspedInMinutes > 1) {
+            tr("You've practiced for " + timeElaspedInMinutes + " minutes! Keep going!");
+        }
+        else if (timeElaspedInMinutes == 1) {
+            tr("A minute and " + timeElaspedInSeconds + "seconds in! Good job!");
+        }
+        else {
+            tr("Relax! Play anything you'd like! You're " + timeElaspedInSeconds + " seconds in.");
+        }
+
+        timeRemainsInMinutes = timeRemains.minutes();
+        timeRemainsInHours = timeRemains.hours();
+
+        if (timeRemainsInHours > 1) {
+            te("You have about " + timeRemainsInHours + " hours and " + timeRemainsInMinutes + " minutes left.");
+        }
+        else if (timeRemainsInHours == 1) {
+            te("You have about an hour left.");
+        }
+        else if (timeRemainsInMinutes > 1) {
+            te("You have about " + timeRemainsInMinutes + " minutes left.");
+        }
+        else if (timeRemainsInMinutes == 1) {
+            te("You have about a minute left.");
+        }
+        else {
+            te("Less than a minute!");
+        }
+
+        if (Session.get('noteElem') !== '-') {
+            timeElapsed.add(interval);
+        }
+
+        timeRemains.subtract(interval);
+        
+        if (Math.floor(timeRemains.asSeconds()) < 1) {
+
+            clearInterval(tid);
+
+            te("Times up! You did great!");
+
+            var totalPoints = timeElaspedInHours * 3600 + timeElaspedInMinutes * 60 + timeElaspedInSeconds;
+
+            tr("You earned " + totalPoints + " points!");
+
+            timerIsRunning = false;
+        }
+    }
+};
+
 Template.timeForm.events({
-    'change .clockpickerform'(event, instance){
+    'change .input-group'(event, instance){
         var endTimeDate = moment($("#time").val(), "hh:mmaa");
         if (endTimeDate.isBefore(moment())){
             endTimeDate.add(1,'days');
@@ -59,151 +165,23 @@ Template.timeForm.events({
     'submit' (event){
         event.preventDefault();
     },
-    'click button'(event){
-
+    'click button'(event, instance){
+        instance.showForm.set(false);
+        initializeTimers(moment()
+            .add(instance.hours.get(),"hours")
+            .add(instance.minutes.get(),"minutes"));
+    },
+    'change #input-hours'(event, instance){
+        instance.hours.set($("#input-hours").val());
+        $("#time").val(moment().add(instance.hours.get(),"hours").add(instance.minutes.get(),"minutes")
+            .format("hh:mmA"));
+    },
+    'change #input-minutes'(event, instance){
+        instance.minutes.set($("#input-minutes").val());
+        $("#time").val(moment().add(instance.hours.get(),"hours").add(instance.minutes.get(),"minutes")
+            .format("hh:mmA"));
     }
 });
-
-Template.soundIndicator.helpers({
-    hasSound: function(){
-        return Session.get('noteElem') !== '-';
-    }
-});
-
-/*
-
-Template.clockpicker.onRendered(function(){
-
-    var cp = $(".clockpicker");
-
-
-    if (timerIsRunning == false){
-        cp.clockpicker({
-            donetext: 'Let\'s start practice!',
-            
-            autoclose : false,
-            
-            default: 'now',
-            
-            fromnow: 60000,
-            
-            twelvehour: true,
-
-            afterDone:function(){
-                var targetTimeStr = moment().format('YYYY\/MM\/DD ')
-                    + $("#time").val()
-                    + moment().format(':ss');
-
-                var timestamp = moment(targetTimeStr,"YYYY\/MM\/DD HH:mm:ss");
-
-                if (timestamp.isBefore(moment())){
-                    timestamp.add(1,'days');
-                }
-
-                initializeTimers(timestamp);
-
-                Scores.insert({
-                    userId: Meteor.userId(),
-                    score: 0,
-                    startDate: new Date()
-                }, function(err, _id){
-                    scoreRecordId = _id;
-                });
-
-
-                $(".clockpickerform").hide();
-                timerIsRunning = true;
-            }
-        });
-    } else{
-        $(".clockpickerform").hide();
-    }
-});
-
-    var initializeTimers = function(timestamp) {
-
-        var timeElapsed = moment.duration(0, 'seconds');
-        var timeRemains = moment.duration(timestamp.diff(moment()));
-        var interval = 200;
-        var tid = setInterval(startCounting, interval);
-
-        var timeElaspedInSeconds;
-        var timeElaspedInMinutes;
-        var timeElaspedInHours;
-
-        var timeRemainsInMinutes;
-        var timeRemainsInHours;
-
-        function te(string) {
-            $("#timeElapsed").html(string);
-        }
-
-        function tr(string) {
-            $("#timeRemains").html(string);
-        }
-
-        function startCounting() {
-
-            timeElaspedInSeconds = timeElapsed.seconds();
-            timeElaspedInMinutes = timeElapsed.minutes();
-            timeElaspedInHours = timeElapsed.hours();
-
-            if (timeElaspedInHours > 1) {
-                tr("You've practiced for " + timeElaspedInHours + " hours and " + timeElaspedInMinutes + " minutes! That's amazing!");
-            }
-            else if (timeElaspedInHours == 1) {
-                tr("You've practiced for an hour and " + timeElaspedInMinutes + " minutes!");
-            }
-            else if (timeElaspedInMinutes > 1) {
-                tr("You've practiced for " + timeElaspedInMinutes + " minutes! Keep going!");
-            }
-            else if (timeElaspedInMinutes == 1) {
-                tr("A minute and " + timeElaspedInSeconds + " in! Good job!");
-            }
-            else {
-                tr("Relax! Play anything you'd like! You're " + timeElaspedInSeconds + " seconds in.");
-            }
-
-            timeRemainsInMinutes = timeRemains.minutes();
-            timeRemainsInHours = timeRemains.hours();
-
-            if (timeRemainsInHours > 1) {
-                te("You have about " + timeRemainsInHours + " hours and " + timeRemainsInMinutes + " minutes left.");
-            }
-            else if (timeRemainsInHours == 1) {
-                te("You have about an hour left.");
-            }
-            else if (timeRemainsInMinutes > 1) {
-                te("You have about " + timeRemainsInMinutes + " minutes left.");
-            }
-            else if (timeRemainsInMinutes == 1) {
-                te("You have about a minute left.");
-            }
-            else {
-                te("Less than a minute!");
-            }
-
-            if (Session.get('noteElem') !== '-') {
-                timeElapsed.add(interval);
-            }
-
-            timeRemains.subtract(interval);
-
-            if (Math.floor(timeRemains.asSeconds()) == 0) {
-
-                clearInterval(tid);
-
-                te("Times up! You did great!");
-
-                var totalPoints = timeElaspedInHours * 3600 + timeElaspedInMinutes * 60 + timeElaspedInSeconds;
-
-                tr("You earned " + totalPoints + " points!");
-
-                timerIsRunning = false;
-            }
-        }
-    };
-
 
 
 var insertScoreToDB = function(){
@@ -215,6 +193,3 @@ var insertScoreToDB = function(){
 
     });
 };
-
-*/
-
